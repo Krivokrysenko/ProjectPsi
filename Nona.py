ad13a59534591e07d58e59af79ea0f02b8f9750d
Krivokrysenko <ckrivokrysenko@gmail.com>
2021-10-13 02:17:50 -0400

    don't code at 2 AM

--- Nona.py
+++ Nona.py
@@ -1,2 +1,14 @@
+# should probably make an init and all and stuff but just manual import for now
+from agents import AlArm
+
+# fun stuff to make agents go brrrrrrrrr
+
+# i was gonna put like, data structures here or something
+alarm = AlArm.alarm()
+
+# Nona's jazz
+
 def summonAgent():
     return None
+
+alarm
---
+++ agents/AlArm.py
@@ -0,0 +1,5 @@
+# i should put stuff here to make things go brrrrrrrr
+
+# then the functions like
+def alarm():
+    print("beep beep")
