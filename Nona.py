2349d4da75e91841ce952ae018d06613af81329e
Krivokrysenko <ckrivokrysenko@gmail.com>
2021-11-10 02:32:02 -0500

    agents can now be loaded/unloaded from config file, as well as imported dynamically, including in that order

--- Nona.py
+++ Nona.py
@@ -1,6 +1,24 @@
-from agents.AlArm import AlArm
+import configparser
+from importlib import import_module
+
+# code to dynamically import agents, might need to clean this up/move it
+
+loadedmods = {}
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+for agent in config["agents"]:
+    loadedmods[agent] = import_module(config["agents"][agent], "agents")
+
+# import enum codes
+
+from agents.agent import Code
+
+# uh dunno what i'm doing with this yet
 
 agentKeywords = []
+
+# Nona go brrrrrrrr
 
 def acceptInput(userstring):
     tokens = userstring.split(" ")
@@ -8,13 +26,14 @@ def acceptInput(userstring):
 
 def summonAgent(tokens):
     if "alarm" in tokens:
+        AlArm = getattr(loadedmods["alarm"], "AlArm")
         alarm = AlArm()
-        moreInfoNeeded, output = alarm.interpret(tokens)
-        # some sort of control flow to call requestFromUser versus outputToUser based on the signal
-        if moreInfoNeeded:
-            requestFromUser(output)
-        else:
-            outputToUser(output)
+        code, output = alarm.interpret(tokens)
+        match code:
+            case Code.INFO:
+                requestFromUser(output)
+            case Code.OUT:
+                outputToUser(output)
 
 def requestFromUser(request):
     outputToUser(request)
@@ -24,10 +43,22 @@ def requestFromUser(request):
 def outputToUser(output):
     print(output)
 
-def loadAgent():
-    return None
+def loadAgent(agentName, filename):
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    config["agents"][agentName] = filename
+    with open('config.ini', 'w') as configfile:
+        config.write(configfile)
+    loadedmods[agentName] = import_module(filename, "agents")
 
-def unloadAgent():
-    return None
+def unloadAgent(agentName, filename):
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    config["agents"][agentName] = filename
+    with open('config.ini', 'w') as configfile:
+        config.write(configfile)
 
+# """testing"""
+
+# loadAgent("alarm", ".AlArm")
 acceptInput(input())
--- agents/agent.py
+++ agents/agent.py
@@ -1,10 +1,16 @@
+from enum import Enum, auto
+
+class Code(Enum):
+    INFO = auto()
+    OUT = auto()
+
 class Agent:
 
     def interpret(self, tokens):
         return None
 
     def requestMoreInfo(self, request):
-        return 1, request
+        return Code.INFO, request
 
     def outputToNona(self, output):
-        return 0, output
+        return Code.OUT, output
